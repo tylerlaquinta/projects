120d683efad3c2ab85260245535f856260f57f67
tylerlaquinta <75007776+tylerlaquinta@users.noreply.github.com>
2021-09-10 11:52:09 -0400

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,41 +1,42 @@
-''' Alien Invasion '''
-''' A Ship That Fires Bullets '''
-
-
-''' Creating a Pygame Window and Responding to User Input '''
-
 import sys
+
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
-    ''' Overall class to manage game assets and behavior '''
+    """Overall class to manage game assets and behavior."""
 
     def __init__(self):
-        ''' Initializing the hame, and create game resources '''
+        """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        # set the background color
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
+
+
 
     def run_game(self):
-        ''' Start the main loop for the game '''
+        """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events
+            # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # redraw the screen during each pass through the loop
-            self.screen.fill(self.bg_color)
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
-            # Make the most recently drawn screen visible
+            # Make the most recently drawn screen visible.
             pygame.display.flip()
 
-
 if __name__ == '__main__':
-    # Make a game instance, and run the game
+    # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
