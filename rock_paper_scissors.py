3eea9f958cabfe337573ff6089199171073cf4e4
tylerlaquinta <75007776+tylerlaquinta@users.noreply.github.com>
2021-09-13 21:33:33 -0400

    Updated

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,7 +3,7 @@ import random, sys
 
 print("""Let's play Rock, Paper, Scissors
 Choose (r)ock, (p)aper of (s)cissors
-Enter 'quit' at any time to quit""") 
+Type 'q' at any time to quit""") 
 
 wins = 0
 ties = 0
@@ -11,31 +11,39 @@ loses = 0
 while True:
     score = f"Wins: {wins} Ties: {ties} Loses: {loses}"
     print(score)
-    choice = input("Choose: ")
-    computer = random.randint(1,3)
-    if choice == 'quit':
-        sys.exit()
-    elif choice == 'r':
-        print("You Chose Rock")
-    elif choice == 'p':
-        print("You Chose Paper")
-    elif choice == 's':
-        print("You Chose Scissors")
-
+    while True:
+        choice = input("Choose: ")
+        computer = random.randint(1,3)
+        if choice == 'quit':
+            sys.exit()
+        elif choice == 'r' or choice == 'p' or choice == 's':
+            break
+        else:
+            print("Please type 'r', 'p', 's', or 'q'")
+    
     if choice == 'r':
         choice = 1
+        print("You Chose Rock")
     elif choice == 'p':
         choice = 2
+        print("You Chose Paper")
     elif choice == 's':
         choice = 3
+        print("You Chose Scissors")
+
+
+    if computer == 1:
+        print("I chose Rock")
+    elif computer == 2:
+        print("I chose Paper")
+    elif computer == 3:
+        print("I chose Scissors")
 
 
     if choice == computer:
         print("It's a draw!")
         ties += 1
-
-    
-    if choice == 1 and computer == 2:
+    elif choice == 1 and computer == 2:
         print("Rock loses to Paper!")
         loses += 1
     # rock beats scissors
